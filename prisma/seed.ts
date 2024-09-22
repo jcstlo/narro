416691b81498e326cd75865f31951df524b6b54d
Joseph Castillo <joseph.cast0226@gmail.com>
2024-09-22 14:19:50 -0700

    Create a Spaces model to separate links by context/project

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  const newSpaceA = await prisma.space.create({
+    data: {
+      name: "Space A",
+    }
+  });
+
+  const newSpaceB = await prisma.space.create({
+    data: {
+      name: "Space B",
+    }
+  });
+
   const links = [
     {
       title: "Link 1",
@@ -17,21 +29,50 @@ async function main() {
     },
   ]
 
-  const new_links = links.map(async (link) => {
+  const newLinksSpaceA = links.map(async (link) => {
     const new_link = await prisma.link.create({
       data: {
         title: link.title,
         url: link.url,
+        spaces: {
+          connect: {
+            id: newSpaceA.id,
+          }
+        },
       },
     });
 
     return new_link;
   });
 
-  new_links.map(async (new_link) => {
+  const newLinksSpaceB = links.map(async (link) => {
+    const new_link = await prisma.link.create({
+      data: {
+        title: link.title,
+        url: link.url,
+        spaces: {
+          connect: {
+            id: newSpaceB.id,
+          }
+        },
+      },
+    });
+
+    return new_link;
+  });
+
+  console.log(newSpaceA);
+  console.log(newSpaceB);
+
+  newLinksSpaceA.map(async (new_link) => {
     const link = await new_link;
     console.log(link);
-  })
+  });
+
+  newLinksSpaceB.map(async (new_link) => {
+    const link = await new_link;
+    console.log(link);
+  });
 }
 
 main()
