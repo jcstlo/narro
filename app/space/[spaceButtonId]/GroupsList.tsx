eb3337fc709f2300371754599ae96a2077034672
Joseph Castillo <joseph.cast0226@gmail.com>
2024-09-24 14:24:23 -0700

    Add select all/none buttons

--- app/space/[spaceButtonId]/GroupsList.tsx
+++ app/space/[spaceButtonId]/GroupsList.tsx
@@ -12,6 +12,14 @@ export function GroupsList({
   setCurrentGroups: Dispatch<SetStateAction<currentGroupsState>>,
 }) {
 
+  function setCheckedAllGroups(checked: boolean) {
+    const copyCurrentGroups = [...currentGroups.groupsList];
+    const modified = copyCurrentGroups.map((group) => {
+      return { ...group, checked: checked };
+    })
+    setCurrentGroups({ groupsList: modified });
+  }
+
   function updateGroupsList(groupId: number, checked: boolean) {
     const copyCurrentGroups = [...currentGroups.groupsList];
     for (let i = 0; i < copyCurrentGroups.length; i++) {
@@ -29,6 +37,7 @@ export function GroupsList({
       className="mb-1"
       value={String(group.id)}
       label={group.name}
+      checked={group.checked}
       onChange={(event) => {
         updateGroupsList(group.id, event.currentTarget.checked);
       }}
@@ -36,10 +45,26 @@ export function GroupsList({
   })
 
   return (
-    <Checkbox.Group
-      label="Groups"
-    >
+    <>
+      <div className="flex">
+        <button
+        className="border border-black px-2 py-1 ml-1 mb-2"
+          onClick={() => {
+            setCheckedAllGroups(true);
+          }}
+        >
+          Select all
+        </button>
+        <button
+        className="border border-black px-2 py-1 ml-1 mb-2"
+          onClick={() => {
+            setCheckedAllGroups(false);
+          }}
+        >
+          Select none
+        </button>
+      </div>
       {checkboxItems}
-    </Checkbox.Group>
+    </>
   )
 }
