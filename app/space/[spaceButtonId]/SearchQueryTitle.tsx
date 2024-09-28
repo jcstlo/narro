3ed2d54257cd13a03cf90b3fdf94ad9465b355d3
Joseph Castillo <joseph.cast0226@gmail.com>
2024-09-28 16:11:28 -0700

    Minor spacing change

--- app/space/[spaceButtonId]/SearchQueryTitle.tsx
+++ app/space/[spaceButtonId]/SearchQueryTitle.tsx
@@ -24,7 +24,7 @@ export function SearchQueryTitle({
         name="searchQueryTitle"
         type="text"
         placeholder="Search title..."
-        className="block w-full rounded-md border border-gray-200 py-2 pl-2 text-sm outline-2 placeholder:text-gray-500 mb-4"
+        className="block w-full rounded-md border border-gray-200 py-2 pl-2 text-sm outline-2 placeholder:text-gray-500 mb-2"
         onChange={onSearchQueryTitleChange}
       />
     </>
