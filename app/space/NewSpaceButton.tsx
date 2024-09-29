5ace30e2c03da6f991dd2e81e02c83db74b34def
Joseph Castillo <joseph.cast0226@gmail.com>
2024-09-28 20:01:23 -0700

    Hide new space button on smaller screen sizes

--- app/space/NewSpaceButton.tsx
+++ app/space/NewSpaceButton.tsx
@@ -7,7 +7,7 @@ export function NewSpaceButton() {
   return (
     <div>
       <button
-        className="border border-black px-2 py-2 mx-1"
+        className="border border-black px-2 py-2 mx-1 hidden md:inline-block"
         onClick={() => {
           router.push("/create/space");
         }}
