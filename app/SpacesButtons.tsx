697516ea0f14874c2a0b1ccbe5590ab1c782a738
Joseph Castillo <joseph.cast0226@gmail.com>
2024-10-01 12:30:21 -0700

    Highlight current space in header

--- app/SpacesButtons.tsx
+++ app/SpacesButtons.tsx
@@ -1,10 +1,18 @@
 import { Space } from "@prisma/client";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+
+function getCurrentSpaceId(pathname: string) {
+  const splitPath = pathname.split("/");
+  const currentSpaceId = splitPath[2];
+  return Number(currentSpaceId);
+}
 
 export function SpacesButtons() {
   const [spacesArray, setSpacesArray] = useState([] as Space[]);
   const router = useRouter();
+  const pathname = usePathname();
+  const currentSpaceId = getCurrentSpaceId(pathname);
 
   useEffect(() => {
     fetch("/api/query/spaces", {
@@ -14,14 +22,24 @@ export function SpacesButtons() {
       .then((data) => {
         setSpacesArray(data);
       })
-}, [])
+  }, [])
+
+  let allSpaceTailwindClasses = "border border-black px-2 py-2 mx-1"
+  if (currentSpaceId === 0) {
+    allSpaceTailwindClasses += " underline font-bold"
+  }
 
   return (
     <div>
       {spacesArray.map((space) => {
+        let tailwindClasses = "border border-black px-2 py-2 mx-1"
+        if (currentSpaceId === space.id) {
+          tailwindClasses += " underline font-bold"
+        }
+
         return <button
           key={space.id}
-          className="border border-black px-2 py-2 mx-1"
+          className={tailwindClasses}
           onClick={() => {
           router.push(`${space.id}`);
         }}
@@ -30,7 +48,7 @@ export function SpacesButtons() {
         </button>
       })}
       <button
-        className="border border-black px-2 py-2 mx-1"
+        className={allSpaceTailwindClasses}
         onClick={() => {
           router.push(`0`);
         }}
