c5e0dca8523a0b10ee49366c1ebd7949e96157bb
Joseph Castillo <joseph.cast0226@gmail.com>
2024-10-04 19:54:43 -0700

    Sort spaces alphabetically in SpacesButtons

--- app/api/query/spaces/route.ts
+++ app/api/query/spaces/route.ts
@@ -1,7 +1,11 @@
 import prisma from "../../../lib/db";
 
 export async function POST() {
-  const allSpaces = await prisma.space.findMany();
+  const allSpaces = await prisma.space.findMany({
+    orderBy: {
+      name: 'asc',
+    }
+  });
 
   return new Response(JSON.stringify(allSpaces));
 }
